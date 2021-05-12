f4cfd17311e0728e004e0b524c9a16eb6759fce9
Nick Kirby <nick.kirby@purplebricks.com>
2021-05-12 19:51:55 +0100

    Implement search

--- src/PodcastNotifications.Frontend/pages/podcasts/index.js
+++ src/PodcastNotifications.Frontend/pages/podcasts/index.js
@@ -7,15 +7,44 @@ export default function Podcasts() {
 
     const router = useRouter();
     const [podcasts, setPodcasts] = useState([]);
+    const [filteredPodcasts, setFilteredPodcasts] = useState([]);
 
     useEffect(() => {
         getPodcasts();
     }, []);
 
+    async function search(input) {
+        if (input.length >= 3) {
+            let filtered = [];
+
+            podcasts.forEach(index => {
+                let podcastsForIndex = []
+                index.data.forEach(podcast => {
+                    if (podcast.title.toLowerCase().includes(input.toLowerCase())) {
+                        podcastsForIndex.push(podcast)
+                    }
+                })
+                if (podcastsForIndex.length > 0) {
+                    filtered.push({
+                        index: index.index,
+                        data: podcastsForIndex
+                    })
+                }                
+            });
+            if (filtered.length > 0){
+                console.log(filtered)
+                setFilteredPodcasts(filtered);
+            }           
+        } else {
+            setFilteredPodcasts(podcasts);
+        }
+    }
+
     async function getPodcasts() {
         const result = await fetch('https://podcastnotifications.blob.core.windows.net/podcasts/index.json')
             .then(response => response.json());
         setPodcasts(result.items);
+        setFilteredPodcasts(result.items);
     }
 
     return (
@@ -38,9 +67,21 @@ export default function Podcasts() {
                                 Click on a podcast below to subscribe for email notifications.
                             </p>
                         </div>
+                        <div className="mt-12">
+                            <div className="mt-1">
+                                <input
+                                    type="text"
+                                    name="email"
+                                    id="email"
+                                    className="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                                    placeholder="search podcasts"
+                                    onChange={(e) => search(e.target.value)}
+                                />
+                            </div>
+                        </div>
 
                         <nav className="mt-10 h-full overflow-y-auto" aria-label="Podcasts">
-                            {podcasts && podcasts.map((char, i) => {
+                            {filteredPodcasts && filteredPodcasts.map((char, i) => {
                                 return (
                                     <div className="relative" key={i}>
                                         <div className="z-10 sticky top-0 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
@@ -50,7 +91,7 @@ export default function Podcasts() {
                                             {char.data && char.data.map((podcast, i) => {
                                                 return (
                                                     <li className="bg-white cursor-pointer" onClick={() => router.push(`/podcasts/subscribe/${podcast.id}/${podcast.title}`)} key={i}>
-                                                        <div className="relative px-6 py-5 flex items-center space-x-3 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+                                                        <div className="relative px-6 py-5 flex items-center space-x-3 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-blue-500">
                                                             <div className="flex-shrink-0">
                                                                 <img className="h-10 w-10 rounded-full" src={podcast.imageUrl} alt="" />
                                                             </div>
@@ -69,7 +110,6 @@ export default function Podcasts() {
                                                     </li>
                                                 )
                                             })}
-
                                         </ul>
                                     </div>
                                 )
